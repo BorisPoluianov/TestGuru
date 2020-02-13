365ae3e66f078ee4602c6a6b49ef112283aabcd7
Boris Poluianov <boris.poluianov@outlook.com>
2020-02-13 20:59:12 +0300

    Added primary models associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :test_progresses
+  has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :tests
+  has_many :test_progresses
 end
