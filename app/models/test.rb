81cba7198e32f49419ab161f5093bf977e6fae1c
Boris Poluianov <boris.poluianov@outlook.com>
2020-02-15 18:21:32 +0300

    Added display_tests_title_by_desc(category) Test class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,4 +3,11 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :test_progresses
   has_many :users
+
+  def self.display_tests_title_by_desc(category)
+    Test.joins(:category)
+      .where(categories: { title: category })
+      .order(title: :DESC)
+      .pluck(:title)
+  end
 end
