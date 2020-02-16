702a7034108094ec9c07ceccc981eaf1d8f6fd3b
Boris Poluianov <boris.poluianov@outlook.com>
2020-02-16 22:26:02 +0300

    Added Author relationship between User and Test

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_02_13_175026) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -69,4 +71,5 @@ ActiveRecord::Schema.define(version: 2020_02_13_175026) do
   add_foreign_key "test_progresses", "tests"
   add_foreign_key "test_progresses", "users"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
