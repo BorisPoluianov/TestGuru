c9f97fe2c2439c0223dff891b2b3315b4b43d427
Boris Poluianov <boris.poluianov@outlook.com>
2020-02-17 14:53:48 +0300

    Fixed: add author reference moved to separate migration file

--- db/migrate/20200210214515_add_references.rb
+++ db/migrate/20200210214515_add_references.rb
@@ -1,7 +1,6 @@
 class AddReferences < ActiveRecord::Migration[6.0]
   def change
     add_reference :tests, :category, foreign_key: true
-    add_reference :tests, :author, foreign_key: { to_table: :users }
     add_reference :questions, :test, foreign_key: true
     add_reference :answers, :question, foreign_key: true
   end
