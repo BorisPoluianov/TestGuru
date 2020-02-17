6fa65354f8a38566ce208cb49856685023569f00
Boris Poluianov <boris.poluianov@outlook.com>
2020-02-17 23:13:53 +0300

    Added & implemented scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,10 +5,18 @@ class Test < ApplicationRecord
   has_many :test_progresses
   has_many :users, through: :test_progresses
 
+  scope :level, -> { where(level: true) }
+  scope :junior_level, -> { where(level: 0..1) }
+  scope :middle_level, -> { where(level: 2..4) }
+  scope :senior_level, -> { where(level: 5..30) }
+  scope :thor_heyerdahl_level, -> { where(level: 30..Float::INFINITY)}
+  scope :by_category, -> (category) {
+    joins(:category)
+    .where(categories: { title: category} )
+    .order(title: :ASC)
+  }
+
   def self.display_tests_title_by_desc(category)
-    Test.joins(:category)
-      .where(categories: { title: category })
-      .order(title: :DESC)
-      .pluck(:title)
+    Test.by_category(category).pluck(:title).reverse
   end
 end
