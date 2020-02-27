081333d200eed940147a8d79cff2a4b8142afc42
Boris Poluianov <boris.poluianov@outlook.com>
2020-02-27 18:10:15 +0300

    Added question_header_for questions_helper method

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+  QUESTIONS_HEADERS = {
+    new: -> (test_title) { "Create new #{test_title} question" },
+    edit: -> (test_title) { "Edite #{test_title} question" }
+  }
+
+  def question_header_for(action)
+    QUESTIONS_HEADERS[action].call(@question.test.title)
+  end
 end
