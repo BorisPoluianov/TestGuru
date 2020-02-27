08aa9664c2655f7dcfef03b2bd3918bf279a5eb8
Boris Poluianov <boris.poluianov@outlook.com>
2020-02-27 16:52:01 +0300

    Created TestsController. Added primary action views to Tests & Questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,32 +1,34 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index new create]
-  before_action :find_question, only: %i[show destroy]
+  before_action :find_test, only: %i[new create]
+  before_action :find_question, only: %i[show edit update destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index
-    render plain: @test.questions.pluck(:id,:body)
-  end
-
-  def show
-    render plain: @question.inspect
-  end
-
   def new
+    @question = @test.questions.build
   end
 
   def create
     @question = @test.questions.build(question_params)
+
     if @question.save
-      render inline: '<%= @question.body %> question was saved.'
+      redirect_to @question.test
     else
       render :new
     end
   end
 
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @question.destroy
-    render plain: 'Question was deleted.'
+    redirect_to @question.test
   end
 
 private
