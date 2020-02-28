505831fb0ef5f7df869e4333f113f3a15cff5b25
Boris Poluianov <boris.poluianov@outlook.com>
2020-02-28 18:48:43 +0300

    Fixed: made easy question_header question_helper method

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,10 +1,10 @@
 module QuestionsHelper
-  QUESTIONS_HEADERS = {
-    new: -> (test_title) { "Create new #{test_title} question" },
-    edit: -> (test_title) { "Edite #{test_title} question" }
-  }
 
-  def question_header_for(action)
-    QUESTIONS_HEADERS[action].call(@question.test.title)
+  def question_header(question)
+    if question.persisted?
+      "Edite #{question.test.title} question"
+    else
+      "Create new #{question.test.title} question"
+    end
   end
 end
