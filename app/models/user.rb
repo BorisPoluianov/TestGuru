a3913b7fa800b970ab92c5acbc91de463479f717
Boris Poluianov <boris.poluianov@outlook.com>
2020-03-02 20:05:04 +0300

    Added users registration, user's password & email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
-  validates :email, presence: true
-
   has_many :test_progresses
   has_many :tests, through: :test_progresses
   has_many :authored_tests, class_name: 'Test', foreign_key: :author_id
+
+  validates :email,
+    format: { with: URI::MailTo::EMAIL_REGEXP },
+    uniqueness: true
+  validates :password, presence: true
+
+  has_secure_password
 
   def display_tests_by_level(level)
     Test.joins(:test_progresses)
