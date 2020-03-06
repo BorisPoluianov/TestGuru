8d241b440535ef92f20dfa5cf1761531e7a09a45
Boris Poluianov <boris.poluianov@outlook.com>
2020-03-06 18:08:22 +0300

    feat(lang): added ru locale, fixed style

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,9 +2,9 @@ module QuestionsHelper
 
   def question_header(question)
     if question.persisted?
-      t('.header_edit', title: question.test.title)
+      t('.header', title: question.test.title)
     else
-      t('.header_new', title: question.test.title)
+      t('.header', title: question.test.title)
     end
   end
 end
