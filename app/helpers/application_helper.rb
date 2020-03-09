e7fe0107b9acad7cb9dc55381971fed0baaac3c2
Boris Poluianov <boris.poluianov@outlook.com>
2020-03-09 15:05:24 +0300

    feat(bootstrap): improved flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,11 @@
 module ApplicationHelper
+  FLASH_TYPES = {
+    notice: "alert alert-info",
+    alert: "alert alert-danger"
+  }.freeze
+
 
   def flash_class(type)
-    case type
-      when :notice then "alert alert-info"
-      when :success then "alert alert-success"
-      when :error then "alert alert-danger"
-      when :alert then "alert alert-warning"
-      else type.to_s
-    end
+    FLASH_TYPES[type]
   end
-
 end
