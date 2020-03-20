3cfb2526fd4a8596a766a035e1a8fa6e6745346d
Boris Poluianov <boris.poluianov@outlook.com>
2020-03-20 21:56:33 +0300

    feat(psql): changed DBMS from Sqlite3 to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_09_170305) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false
